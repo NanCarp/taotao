6b8793fd54c5f6e355b94be9c2f60e1160d85db6
nanliyu <nancarpli@163.com>
2017-09-12 22:44:41 +0800

    Initial commit

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,14 @@
+package com.taotao.service;
+
+import org.springframework.stereotype.Service;
+
+import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.pojo.TbItem;
+
+@Service
+public interface ItemService {
+	
+	TbItem getItemById(long itemId);
+	
+	EUDataGridResult getItemList(int page, int rows);
+}
---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -0,0 +1,34 @@
+package com.taotao.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+
+@RequestMapping("/item")
+@Controller
+public class ItemController {
+	
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/{itemId}")
+	@ResponseBody
+	public TbItem getItemById(@PathVariable long itemId) {
+		TbItem tbItem = itemService.getItemById(itemId);
+		return tbItem;
+	}
+	
+	@RequestMapping("/list")
+	@ResponseBody
+	public EUDataGridResult getItemList(Integer page, Integer rows) {
+		EUDataGridResult result = itemService.getItemList(page, rows);
+		return result;
+	}
+	
+}
