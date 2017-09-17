25f59b644fc7d5af44e4941e2937a16fa6c56a09
nanliyu <nancarpli@163.com>
2017-09-17 12:12:56 +0800

    1

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -3,6 +3,7 @@ package com.taotao.service;
 import org.springframework.stereotype.Service;
 
 import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.common.utils.TaotaoResult;
 import com.taotao.pojo.TbItem;
 
 @Service
@@ -11,4 +12,6 @@ public interface ItemService {
 	TbItem getItemById(long itemId);
 	
 	EUDataGridResult getItemList(int page, int rows);
+	
+	TaotaoResult createItem(TbItem tbItem);
 }
